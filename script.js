87fc0c51dc30e63fb2bfd8bd6c941d167dd5de57
Abhay Pratap Singh <abhaypratapninth@gmail.com>
2024-05-16 21:12:48 +0530

    Complete site

--- script.js
+++ script.js
@@ -23,13 +23,30 @@ document.addEventListener("mousemove", (e) => {
   document.body.style.setProperty("--y", `${e.clientY}px`);
 });
 
-document.querySelectorAll("a").forEach((a) => {
-  a.addEventListener("mouseover", () => {
+document.querySelectorAll(".visual, .video").forEach((a) => {
+  a.addEventListener("mouseover", (e) => {
     document.body.style.setProperty("--cursor-bkd-big", `blue`);
     document.body.style.setProperty("--cursor-bkd-small", `white`);
+    if (e.target.classList.contains("video"))
+      document.body.style.setProperty("--cursor-content", `'play'`);
+    else document.body.style.setProperty("--cursor-content", `'view'`);
   });
   a.addEventListener("mouseout", () => {
     document.body.style.setProperty("--cursor-bkd-big", `transparent`);
     document.body.style.setProperty("--cursor-bkd-small", `black`);
+    document.body.style.setProperty("--cursor-content", `''`);
+  });
+});
+
+document.querySelectorAll("a").forEach((a) => {
+  a.addEventListener("mouseover", () => {
+    document.body.style.setProperty("--cursor-bkd-big", `black`);
+    document.body.style.setProperty("--cursor-bkd-small", `white`);
+    document.body.style.setProperty("--cursor-big-width", `20px`);
+  });
+  a.addEventListener("mouseout", () => {
+    document.body.style.setProperty("--cursor-bkd-big", `transparent`);
+    document.body.style.setProperty("--cursor-bkd-small", `black`);
+    document.body.style.setProperty("--cursor-big-width", `40px`);
   });
 });
